c0ef9018c6885e02e2cd0ccd5d322ee0a8593781
danielmac313 <78310702+danielmac313@users.noreply.github.com>
2022-07-14 01:36:09 +0300

    new upload 01:25 in the night 14/7

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    $db_host = '148.66.138.145';
+    $db_user = 'dbusrShnkr22';
+    $db_pass = 'studDBpwWeb1!';
+    $db_name = 'dbShnkr22studWeb1';
+
+    if(!defined("URL")) define("URL", "http://localhost/mealy/");
+
+?>
